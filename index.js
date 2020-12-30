6768943043bec11c424b3c219488376e3602d54c
DeemoHarlos <cces4415@gmail.com>
2020-12-31 01:21:51 +0800

    index

---
+++ index.js
@@ -0,0 +1,35 @@
+let main = new Vue({
+	el: '#main',
+	data: {
+		characters: [{
+			name: 'Mori Calliope',
+			image: 'assets/calli1.png',
+			size: 1,
+			color: 'F7CEE0'
+		},{
+			name: 'Takanashi Kiara',
+			image: 'assets/kiara1.png',
+			size: 1,
+			color: 'FB8464'
+		},{
+			name: 'Ninomae Ina\'nis',
+			image: 'assets/ina1.png',
+			size: 1,
+			color: '685980'
+		},{
+			name: 'Gawr Gura',
+			image: 'assets/gura1.png',
+			size: 1,
+			color: '34749E'
+		},{
+			name: 'Amelia Watson',
+			image: 'assets/ame1.png',
+			size: 1,
+			color: 'F8DEC7'
+		}]
+	},
+	methods: {
+	},
+	mounted: function() {
+	}
+})
